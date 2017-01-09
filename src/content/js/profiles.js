2a59270c7ddce6812fa1116667584643d37f5d93
Jen Hamon <jhamon@gmail.com>
2017-01-08 22:52:26 -0800

    Hide matching facebook and twitter feed items

--- src/content/js/profiles.js
+++ src/content/js/profiles.js
@@ -7,6 +7,18 @@ const PROFILES = {
     selectors: ['article', 'a', '.breaking-news', '.cn'],
     label: 'cnn'
   },
+  "twitter.com": {
+    selectors: ['.stream-item', '.tweet'],
+    label: 'twitter'
+  },
+  "www.facebook.com": {
+    selectors: ['.userContentWrapper'],
+    label: 'facebook'
+  },
+  "www.metafilter.com": {
+    selectors: ['.copy.post', 'a', '.posttitle', '.copy'],
+    label: 'metafilter'
+  },
   default: {
     selectors: ['article', 'p', 'span', 'em', 'strong', 'h1', 'h2', 'h2', 'h3', 'h4', 'h5'],
     label: 'default'
